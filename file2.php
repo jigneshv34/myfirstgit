9c0b194b43139cb528092ce903301582c215ea0a
jigneshv <jigneshv@aspl.in>
2015-07-03 12:37:26 +0530

    conflict resolved

--- file2.php
+++ file2.php
@@ -4,12 +4,12 @@ include_once('file1.php');
 
 foo\bar() // output: from bar
 
-<<<<<<< HEAD
-echo 'This is file2';
-=======
+
+
+
 echo "This is file 2";
 
 echo 'this is edited second time';
->>>>>>> master
+
 
 ?>
